8cb2fccea429ce299ff8dd2962f5c8c42511f397
Bremda <brenda.s.valadares@gmail.com>
2022-08-19 19:20:30 -0300

    Add edit page, title page, description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_07_134710) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_19_214358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,9 +54,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_07_134710) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
